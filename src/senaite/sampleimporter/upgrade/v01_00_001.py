a399e3aa6f5c149fc6c8686ce4dc845c67b17bc2
luthando5 <luthando@bikalabs.com>
2023-01-09 16:00:46 +0200

    Update upgrade script

--- src/senaite/sampleimporter/upgrade/v01_00_001.py
+++ src/senaite/sampleimporter/upgrade/v01_00_001.py
@@ -1,28 +1,8 @@
 # -*- coding: utf-8 -*-
-#
-# This file is part of SENAITE.QUEUE.
-#
-# SENAITE.QUEUE is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the Free
-# Software Foundation, version 2.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program; if not, write to the Free Software Foundation, Inc., 51
-# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
-# Copyright 2019-2021 by it's authors.
-# Some rights reserved, see README and LICENSE.
-
-from senaite.sampleimporter import PRODUCT_NAME
-from senaite.sampleimporter import logger
 
 from senaite.core.upgrade import upgradestep
-from senaite.core.upgrade.utils import UpgradeUtils
+from senaite.sampleimporter import PRODUCT_NAME
+from senaite.sampleimporter import logger
 from senaite.core.config import PROJECTNAME as product
 
 version = "1.0.1"
@@ -32,20 +12,6 @@ profile = "profile-{0}:default".format(product)
 def upgrade(tool):
     portal = tool.aq_inner.aq_parent
     setup = portal.portal_setup
-    ut = UpgradeUtils(portal)
-    ver_from = ut.getInstalledVersion(PRODUCT_NAME)
-
-    if ut.isOlderVersion(PRODUCT_NAME, version):
-        logger.info(
-            "Skipping upgrade of {0}: {1} > {2}".format(PRODUCT_NAME, ver_from, version)
-        )
-        return True
-
-    logger.info("Upgrading {0}: {1} -> {2}".format(PRODUCT_NAME, ver_from, version))
-
-    # -------- ADD YOUR STUFF BELOW --------
-
     setup.runImportStepFromProfile(profile, "workflow")
-
     logger.info("{0} upgraded to version {1}".format(PRODUCT_NAME, version))
     return True
